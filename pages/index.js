1ec33f43000f2424e9e535ea64a29912fe68c13e
Sergey Grigorenko <sergey.grigorenko@metalamp.io>
2022-08-26 16:14:42 +0300

    added some basic elements

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,14 @@
 import Dropdown from '../components/Dropdown';
 import Layout from '../components/Layout';
+import Search from '../components/Search';
+import SwapSvg from '../public/icons/swap.svg';
+
 export default function Home() {
   return (
     <Layout title="Select your Game!">
       <Dropdown />
-      <h2>Main</h2>
+      <Search />
+      <SwapSvg width={22} />
     </Layout>
   );
 }
